ac706549ad988731ca6d7cb0b2e7d14a3074bd18
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-24 13:27:02 -0500

    Add basic intro page JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 
-function App() {
+const App = () => {
 
+    return (
+        <main>
+            <header></header>
+            <section>
+                <article className='intro'>
+                    <section className='intro-left'>
+                        <h1>Taylor Galloway</h1>
+                        <h2>Front-End Developer</h2>
+                        <div className='intro-button-container'>
+                            <button className='resume-button'></button>
+                        <button className='contact-me-button'></button>
+                        </div>
+                        <div className='socials'>
+
+                        </div>
+                    </section>
+                    <section className='intro-right'>
+                        <p>Something fun</p>
+                    </section>
+                </article>
+            </section>
+        </main>
+    )
 }
 
 export default App;
