9cff61b6cad3dac5d41874cc143848672f0225cf
Taylor Galloway <gallowaytaylor@outlook.com>
2021-10-24 13:29:39 -0500

    Add nav bar JSX

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,23 @@ const App = () => {
 
     return (
         <main>
-            <header></header>
+            <header>
+                <h3>Taylor Galloway</h3>
+                <nav>
+                    <button>About</button>
+                    <button>Skills</button>
+                    <button>Projects</button>
+                    <button>Contact</button>
+                </nav>
+            </header>
             <section>
                 <article className='intro'>
                     <section className='intro-left'>
                         <h1>Taylor Galloway</h1>
                         <h2>Front-End Developer</h2>
                         <div className='intro-button-container'>
-                            <button className='resume-button'></button>
-                        <button className='contact-me-button'></button>
+                            <button className='resume-button'>Resumé</button>
+                            <button className='contact-me-button'>Contact Me</button>
                         </div>
                         <div className='socials'>
 
